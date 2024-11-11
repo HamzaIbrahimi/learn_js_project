d08dd1a4fe5818ecfc482b3b3322cbcf3b9abe3f
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-11 22:15:45 +0100

    Render food elements on submit

--- src/main.js
+++ src/main.js
@@ -10,18 +10,37 @@ formSubmit.addEventListener("submit", (e) => {
   const fat = document.querySelector("#create-fat");
   const fetchWrapper = new FetchWrapper(baseAPI);
   if (selected.value.length != 0) {
-    try {
-      fetchWrapper.post("unique_database", {
+    fetchWrapper
+      .post("/", {
         fields: {
           name: { stringValue: selected.value },
           carbs: { integerValue: +carbs.value },
           protein: { integerValue: +protein.value },
           fat: { integerValue: +fat.value },
         },
+      })
+      .then((data) => {
+        if (data.error) {
+          console.log("Failed Fetch");
+          return;
+        } else {
+          const foodList = document.querySelector("#food-list");
+          foodList.insertAdjacentHTML(
+            "beforeend",
+            `<li class="card">
+            <div>
+              <h3 class="name">${selected.value}</h3>
+              <div class="calories">calories</div>
+              <ul class="macros">
+                <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
+                <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
+                <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+              </ul>
+            </div>
+          </li>`
+          );
+          formSubmit.reset();
+        }
       });
-      formSubmit.reset();
-    } catch (error) {
-      console.error(error);
-    }
   }
 });
