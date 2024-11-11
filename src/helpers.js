43d2a15574fd4ead09d1976a8fe331287f8ae2cc
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-11 22:31:52 +0100

    Fix spelling

--- src/helpers.js
+++ src/helpers.js
@@ -2,13 +2,13 @@ const capitalize = (word) => {
   return word.at(0).toUpperCase() + word.slice(1);
 };
 
-const calculateCalories = (carbs, protien, fat) => {
+const calculateCalories = (carbs, protein, fat) => {
   const carbsProteinCaloriesPerGram = 4;
   const fatCaloriesPerGram = 9;
 
   return (
     carbs * carbsProteinCaloriesPerGram +
-    protien * carbsProteinCaloriesPerGram +
+    protein * carbsProteinCaloriesPerGram +
     fat * fatCaloriesPerGram
   );
 };
