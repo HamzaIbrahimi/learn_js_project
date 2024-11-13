34938bcd37814a5a100f58075531e6b198f5252c
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-14 00:16:14 +0100

    Render database items function

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { capitalize, calculateCalories } from "./helpers.js";
 import "./styles.css";
 import snackbar from "snackbar";
 
+const foodList = document.querySelector("#food-list");
+const fetchWrapper = new FetchWrapper(baseAPI);
 const formSubmit = document.querySelector("#create-form");
 formSubmit.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,7 +13,7 @@ formSubmit.addEventListener("submit", (e) => {
   const carbs = document.querySelector("#create-carbs");
   const protein = document.querySelector("#create-protein");
   const fat = document.querySelector("#create-fat");
-  const fetchWrapper = new FetchWrapper(baseAPI);
+
   if (selected.value.length != 0) {
     fetchWrapper
       .post("/", {
@@ -23,7 +25,6 @@ formSubmit.addEventListener("submit", (e) => {
         },
       })
       .then((data) => {
-        const foodList = document.querySelector("#food-list");
         foodList.insertAdjacentHTML(
           "beforeend",
           `<li class="card">
@@ -58,3 +59,37 @@ formSubmit.addEventListener("submit", (e) => {
       });
   }
 });
+
+const init = () => {
+  fetchWrapper.get("/").then((data) => {
+    data.documents?.forEach((document) => {
+      console.log(document);
+      foodList.insertAdjacentHTML(
+        "beforeend",
+        `<li class="card">
+            <div>
+              <h3 class="name">${document.fields.name.stringValue}</h3>
+              <div class="calories">${calculateCalories(
+                document.fields.carbs.integerValue,
+                document.fields.protein.integerValue,
+                document.fields.fat.integerValue
+              )} calories</div>
+              <ul class="macros">
+                <li class="carbs"><div>Carbs</div><div class="value">${
+                  document.fields.carbs.integerValue
+                }g</div></li>
+                <li class="protein"><div>Protein</div><div class="value">${
+                  document.fields.protein.integerValue
+                }g</div></li>
+                <li class="fat"><div>Fat</div><div class="value">${
+                  document.fields.fat.integerValue
+                }g</div></li>
+              </ul>
+            </div>
+          </li>`
+      );
+    });
+  });
+};
+
+init();
