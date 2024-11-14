48acdb00fdddaea0fba5be917cb49c45e7d5d567
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-14 14:52:07 +0100

    Refactor dublicate code into function

--- src/helpers.js
+++ src/helpers.js
@@ -12,4 +12,26 @@ const calculateCalories = (carbs, protein, fat) => {
     fat * fatCaloriesPerGram
   );
 };
-export { capitalize, calculateCalories };
+
+const displayEntry = (name, carbs, protein, fat) => {
+  const foodList = document.querySelector("#food-list");
+  foodList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(name)}</h3>
+          <div class="calories">${calculateCalories(
+            carbs,
+            protein,
+            fat
+          )} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${carbs}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${protein}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${fat}g</div></li>
+          </ul>
+        </div>
+      </li>`
+  );
+};
+export { displayEntry };
--- src/main.js
+++ src/main.js
@@ -1,53 +1,34 @@
 import FetchWrapper from "./fetch-wrapper.js";
 import { baseAPI } from "./API.js";
-import { capitalize, calculateCalories } from "./helpers.js";
+import { displayEntry } from "./helpers.js";
 import "./styles.css";
 import snackbar from "snackbar";
 
-const foodList = document.querySelector("#food-list");
 const fetchWrapper = new FetchWrapper(baseAPI);
 const formSubmit = document.querySelector("#create-form");
 formSubmit.addEventListener("submit", (e) => {
   e.preventDefault();
-  const selected = document.querySelector("#create-name");
+  const selectedFoodItem = document.querySelector("#create-name");
   const carbs = document.querySelector("#create-carbs");
   const protein = document.querySelector("#create-protein");
   const fat = document.querySelector("#create-fat");
 
-  if (selected.value.length != 0) {
+  if (selectedFoodItem.value.length != 0) {
     fetchWrapper
       .post("/", {
         fields: {
-          name: { stringValue: selected.value },
-          carbs: { integerValue: +carbs.value },
-          protein: { integerValue: +protein.value },
-          fat: { integerValue: +fat.value },
+          name: { stringValue: selectedFoodItem.value },
+          carbs: { integerValue: carbs.value },
+          protein: { integerValue: protein.value },
+          fat: { integerValue: fat.value },
         },
       })
       .then((data) => {
-        foodList.insertAdjacentHTML(
-          "beforeend",
-          `<li class="card">
-            <div>
-              <h3 class="name">${capitalize(selected.value)}</h3>
-              <div class="calories">${calculateCalories(
-                carbs.value,
-                protein.value,
-                fat.value
-              )} calories</div>
-              <ul class="macros">
-                <li class="carbs"><div>Carbs</div><div class="value">${
-                  carbs.value
-                }g</div></li>
-                <li class="protein"><div>Protein</div><div class="value">${
-                  protein.value
-                }g</div></li>
-                <li class="fat"><div>Fat</div><div class="value">${
-                  fat.value
-                }g</div></li>
-              </ul>
-            </div>
-          </li>`
+        displayEntry(
+          selectedFoodItem.value,
+          carbs.value,
+          protein.value,
+          fat.value
         );
         snackbar.show("Food added successfully");
         formSubmit.reset();
@@ -63,30 +44,12 @@ formSubmit.addEventListener("submit", (e) => {
 const init = () => {
   fetchWrapper.get("/").then((data) => {
     data.documents?.forEach((document) => {
-      console.log(document);
-      foodList.insertAdjacentHTML(
-        "beforeend",
-        `<li class="card">
-            <div>
-              <h3 class="name">${document.fields.name.stringValue}</h3>
-              <div class="calories">${calculateCalories(
-                document.fields.carbs.integerValue,
-                document.fields.protein.integerValue,
-                document.fields.fat.integerValue
-              )} calories</div>
-              <ul class="macros">
-                <li class="carbs"><div>Carbs</div><div class="value">${
-                  document.fields.carbs.integerValue
-                }g</div></li>
-                <li class="protein"><div>Protein</div><div class="value">${
-                  document.fields.protein.integerValue
-                }g</div></li>
-                <li class="fat"><div>Fat</div><div class="value">${
-                  document.fields.fat.integerValue
-                }g</div></li>
-              </ul>
-            </div>
-          </li>`
+      const getField = document.fields;
+      displayEntry(
+        getField.name.stringValue,
+        getField.carbs.integerValue,
+        getField.protein.integerValue,
+        getField.fat.integerValue
       );
     });
   });
