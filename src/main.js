846d12f1160fe9e5d1ef300aa244554dbe44ac4d
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-11-14 18:50:40 +0100

    Render chart and add data inputs from AppData class

--- src/main.js
+++ src/main.js
@@ -1,18 +1,67 @@
 import FetchWrapper from "./fetch-wrapper.js";
 import { baseAPI } from "./API.js";
-import { displayEntry } from "./helpers.js";
+import { capitalize, calculateCalories } from "./helpers.js";
 import "./styles.css";
+import AppData from "./app-data.js";
 import snackbar from "snackbar";
+import Chart from "chart.js/auto";
 
+const appData = new AppData();
 const fetchWrapper = new FetchWrapper(baseAPI);
 const formSubmit = document.querySelector("#create-form");
+const foodList = document.querySelector("#food-list");
+let chartInstance = null;
+
+const displayEntry = (name, carbs, protein, fat) => {
+  appData.addFood(carbs, protein, fat);
+  foodList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(name)}</h3>
+          <div class="calories">${calculateCalories(
+            carbs,
+            protein,
+            fat
+          )} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${carbs}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${protein}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${fat}g</div></li>
+          </ul>
+        </div>
+      </li>`
+  );
+};
+
+const renderChart = () => {
+  chartInstance?.destroy();
+  const ctx = document.getElementById("app-chart").getContext("2d");
+  chartInstance = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Carbs", "Protein", "Fat"],
+      datasets: [
+        {
+          label: "Data label",
+          data: [
+            appData.getTotalCarbs(),
+            appData.getTotalProtein(),
+            appData.getTotalFat(),
+          ],
+          backgroundColor: ["#25AEEE", "FECD52", "#57D269"],
+        },
+      ],
+    },
+  });
+};
+
 formSubmit.addEventListener("submit", (e) => {
   e.preventDefault();
   const selectedFoodItem = document.querySelector("#create-name");
   const carbs = document.querySelector("#create-carbs");
   const protein = document.querySelector("#create-protein");
   const fat = document.querySelector("#create-fat");
-
   if (selectedFoodItem.value.length != 0) {
     fetchWrapper
       .post("/", {
@@ -31,6 +80,7 @@ formSubmit.addEventListener("submit", (e) => {
           fat.value
         );
         snackbar.show("Food added successfully");
+        renderChart();
         formSubmit.reset();
       })
       .catch((error) => {
@@ -52,6 +102,7 @@ const init = () => {
         getField.fat.integerValue
       );
     });
+    renderChart();
   });
 };
 
